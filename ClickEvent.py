a51533afc7826df1fc0fefaf26a58673b0a9eab6
Diztenz <cosmoc05@yahoo.com>
2021-04-14 08:35:37 -0700

    041421

--- ClickEvent.py
+++ ClickEvent.py
@@ -1,13 +1,17 @@
 from tkinter import *
+
 window=Tk()
+window.geometry("400x400")
 txt=Entry(window, width=20)
-txt.grid(row=0, column=3)
+txt.grid(row=1, column=0)
 l1=Label(window, text="Smart Programmer", font=15)
-l1.grid(row=6, columns=1)
+l1.grid(row=0, column=0)
+l2=Label(window)
+l2.grid(row=3, column=0)
 def click():
     res="Welcome to " + txt.get()
-    l1.configure(text=res)
+    l2.configure(text=res, font=15)
 
 Btn=Button(window,text="Enter",fg="yellow",bg="blue",command=click)
-Btn.grid(row=5,column=2)
+Btn.grid(row=2,column=0)
 window.mainloop()
